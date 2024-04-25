4af1038960849d83b9fd6bb7f6abbbdfd9d0990b
Navaneethraot <youremail@gmail.com>
2024-04-25 15:32:42 +0530

    expense shell

--- common.sh
+++ common.sh
@@ -22,10 +22,10 @@ VALIDATE(){
 
 check_root(){
     if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
-else
-    echo "You are super user."
-fi
+    then
+        echo "Please run this script with root access."
+        exit 1 # manually exit if error comes.
+    else
+        echo "You are super user."
+    fi
 }
